f296b0349ac60ea33f87c8408459f2f265a23dc7
Andjela <azoric1d@gmail.com>
2026-01-19 16:12:27 +0100

    Initial project setup

---
+++ src/main/java/com/eclept/andjelazoric_eclept_be_labflow/AndjelaZoricEcleptBeLabFlowApplication.java
@@ -0,0 +1,13 @@
+package com.eclept.andjelazoric_eclept_be_labflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndjelaZoricEcleptBeLabFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndjelaZoricEcleptBeLabFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eclept/andjelazoric_eclept_be_labflow/AndjelaZoricEcleptBeLabFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclept.andjelazoric_eclept_be_labflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndjelaZoricEcleptBeLabFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
