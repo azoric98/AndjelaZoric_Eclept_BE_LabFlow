c9a190de489315ad44abb1cdf17c571eb6998aa8
Andjela <azoric1d@gmail.com>
2026-01-19 16:15:54 +0100

    DTO class for TestRequest added.

---
+++ src/main/java/com/eclept/andjelazoric_eclept_be_labflow/dto/TestRequestDTO.java
@@ -0,0 +1,10 @@
+package com.eclept.andjelazoric_eclept_be_labflow.dto;
+
+import lombok.Data;
+
+@Data
+public class TestRequestDTO {
+
+    private Long testTypeId;
+    private boolean walkIn;
+}
