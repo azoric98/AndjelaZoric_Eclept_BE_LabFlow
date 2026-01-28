7392431a3444f89200c4cd48e477f34b89d99398
Andjela <azoric1d@gmail.com>
2026-01-28 17:36:09 +0100

    Minor change

--- src/test/java/com/eclept/andjelazoric_eclept_be_labflow/AndjelaZoricEcleptBeLabFlowApplicationTests.java
+++ src/test/java/com/eclept/andjelazoric_eclept_be_labflow/AndjelaZoricEcleptBeLabFlowApplicationTests.java
@@ -1,13 +1,9 @@
 package com.eclept.andjelazoric_eclept_be_labflow;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AndjelaZoricEcleptBeLabFlowApplicationTests {
+class AndjelaZoricEcleptBeLabFlowApplicationTests{
 
-    @Test
-    void contextLoads() {
-    }
 
 }
